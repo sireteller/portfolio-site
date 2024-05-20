2bbb92ed19db392728c862dae00f5933d4bcedf6
Siret Eller <sireteller@gmail.com>
2024-05-20 23:55:57 +0300

    feat(header): style header & organize css

--- components/header/header.js
+++ components/header/header.js
@@ -2,9 +2,15 @@ class SweetHeader extends HTMLElement {
 	constructor() {
 		super();
 		this.innerHTML = `
-        <header>
-        <h1>Siret Eller</h1>
-        <div class="header-subtitle">Front-end developer & artist</div>
+        <header class="header">
+			<div class="header-stripe">
+				<div class="header-stripe-text">
+        			<h1 class="header-title">Siret Eller</h1>
+        			<div class="header-subtitle">Front-end developer & artist</div>
+				</div>
+			</div>
+			<img class="header-img-overlay" src="./components/header/assets/hero-bg-top.png" alt=""/>
+			<div class="header-frost-overlay"></div>
         </header>`;
 	}
 }
