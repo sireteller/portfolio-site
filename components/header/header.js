13dfd98188954b64663dcda6336bb936fcc11793
Siret Eller <sireteller@gmail.com>
2024-06-18 20:19:53 +0300

    chore: tweak styles

--- components/header/header.js
+++ components/header/header.js
@@ -8,9 +8,8 @@ class SweetHeader extends HTMLElement {
         			<h1 class="header-title">Siret Eller</h1>
         			<div class="header-subtitle">Front-end developer & artist</div>
 				</div>
+				<img class="header-img-overlay" src="./components/header/assets/hero-bg-top.png" alt=""/>
 			</div>
-			<img class="header-img-overlay" src="./components/header/assets/hero-bg-top.png" alt=""/>
-			<div class="header-frost-overlay"></div>
         </header>`;
 	}
 }
