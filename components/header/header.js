93789edebcf8be53d5dab5b7b87461207efed650
Siret Eller <sireteller@gmail.com>
2024-07-12 12:44:08 +0300

    feat(header): update assets

--- components/header/header.js
+++ components/header/header.js
@@ -7,11 +7,12 @@ class SweetHeader extends HTMLElement {
 		this.innerHTML = `
         <header class="header">
 			<div class="header-stripe">
+			<img class="header-img-overlay header-img-overlay--small" src="./components/header/assets/single-lilac.png" alt=""/>
 				<div class="header-stripe-text">
         			<h1 class="header-title">Siret Eller</h1>
         			<div class="header-subtitle">Front-end developer & artist</div>
 				</div>
-				<img class="header-img-overlay" src="./components/header/assets/hero-bg-top.png" alt=""/>
+				<img class="header-img-overlay header-img-overlay--large" src="./components/header/assets/hero-bg.png" alt=""/>
 			</div>
         </header>`;
 	}
