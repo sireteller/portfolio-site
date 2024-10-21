9e1fbaaf3946c00272a9d8bcbc3d59ab16d80ab4
Siret Eller <sireteller@gmail.com>
2024-10-21 21:38:45 +0300

    feat(switcheroo): add keyboard interactivity

--- components/switcheroo/switcheroo.js
+++ components/switcheroo/switcheroo.js
@@ -1,20 +1,18 @@
-let draggerEl;
-let parentEl;
-let resizerEl;
-
-let pointerMoveListener = new AbortController();
-const scrollListener = new AbortController();
-
 class SweetSwitcheroo extends HTMLElement {
 	constructor() {
 		super();
+
+		let draggerEl;
+		let parentEl;
+		let resizerEl;
+		let pointerMoveListener = new AbortController();
+		let scrollListener = new AbortController();
 	}
 
 	connectedCallback() {
-		// TODO a11y - keyboard & screen reader
 		this.innerHTML = `
         <div class="switcheroo">
-			<button class="switcheroo__dragger" aria-label="Drag me"></button>
+			<button class="switcheroo__dragger" aria-label="Drag me to switch between images"></button>
             <div class="switcheroo__resizer">
                 <div class="switcheroo__top-img">
                     <img src="./components/switcheroo/assets/intro-image__drawing.png" alt="A cartoony illustration of me" draggable="false" />
@@ -26,29 +24,34 @@ class SweetSwitcheroo extends HTMLElement {
         </div>
         `;
 
-		draggerEl = this.querySelector(".switcheroo__dragger");
-		parentEl = this.parentElement;
-		resizerEl = this.querySelector(".switcheroo__resizer");
+		this.draggerEl = this.querySelector(".switcheroo__dragger");
+		this.parentEl = this.parentElement;
+		this.resizerEl = this.querySelector(".switcheroo__resizer");
+		this.pointerMoveListener = new AbortController();
+		this.scrollListener = new AbortController();
 
 		this.updateMaxWidth();
 		window.addEventListener("resize", this.updateMaxWidth);
 
-		draggerEl.style.display = "none";
+		this.draggerEl.addEventListener("keydown", (e) => this.onDraggerKeyDown(e));
+
+		this.draggerEl.style.display = "none";
+
 		if (
-			resizerEl.getBoundingClientRect().bottom <= window.innerHeight &&
-			resizerEl.getBoundingClientRect().top >= 0
+			this.resizerEl.getBoundingClientRect().bottom <= window.innerHeight &&
+			this.resizerEl.getBoundingClientRect().top >= 0
 		) {
 			this.animate();
 		} else {
-			window.addEventListener("scroll", this.animate, { signal: scrollListener.signal });
+			window.addEventListener("scroll", this.animate, { signal: this.scrollListener.signal });
 		}
 	}
 
 	updateMaxWidth = () => {
-		if (parentEl.className === "grid-duo") {
-			const parentCompStyle = getComputedStyle(parentEl);
+		if (this.parentEl.className === "grid-duo") {
+			const parentCompStyle = getComputedStyle(this.parentEl);
 
-			resizerEl.style.maxWidth = `${
+			this.resizerEl.style.maxWidth = `${
 				document.documentElement.clientWidth > 720
 					? parseFloat(parentCompStyle.getPropertyValue("width")) / 2 -
 					  parseFloat(parentCompStyle.getPropertyValue("gap"))
@@ -59,57 +62,73 @@ class SweetSwitcheroo extends HTMLElement {
 
 	animate = () => {
 		if (
-			resizerEl.getBoundingClientRect().bottom <= window.innerHeight &&
-			resizerEl.getBoundingClientRect().top >= 0
+			this.resizerEl.getBoundingClientRect().bottom <= window.innerHeight &&
+			this.resizerEl.getBoundingClientRect().top >= 0
 		) {
-			resizerEl.style.animation = "2500ms 1 100ms normal sneak-peek";
+			this.resizerEl.style.animation = "2500ms 1 100ms normal sneak-peek";
 
-			scrollListener.abort();
+			this.scrollListener.abort();
 
 			setTimeout(this.setUpResizing, 2600);
 		}
 	};
 
 	setUpResizing = () => {
-		draggerEl.style.display = "unset";
+		this.draggerEl.style.display = "unset";
 
 		/* Pointer events were recommended on MDN here https://developer.mozilla.org/en-US/docs/Web/API/Touch_events#:~:text=To%20support%20both%20touch%20and%20mouse%20across%20all%20types%20of%20devices%2C%20use%20pointer%20events%20instead.
 		   Supposedly it should support both mouse and touch, but it's super messy on mobile. 
 		   TODO Requires further research what I can do here to support mobile */
-		draggerEl.addEventListener("pointerdown", () => {
-			pointerMoveListener = new AbortController();
+		this.draggerEl.addEventListener("pointerdown", () => {
+			this.pointerMoveListener = new AbortController();
 
-			document.addEventListener("pointermove", (e) => this.resize(e), {
-				signal: pointerMoveListener.signal,
+			document.addEventListener("pointermove", (event) => this.onPointerMove(event), {
+				signal: this.pointerMoveListener.signal,
 			});
 		});
 
 		document.addEventListener("pointerup", () => {
-			pointerMoveListener.abort();
+			this.pointerMoveListener.abort();
 		});
 
 		document.addEventListener("pointercancel", () => {
-			pointerMoveListener.abort();
+			this.pointerMoveListener.abort();
 		});
 	};
 
-	resize = (event) => {
-		const draggedWidth = event.clientX - resizerEl.getBoundingClientRect().left;
-		const maxWidth = parseFloat(resizerEl.style.maxWidth);
-		let newWidth = draggedWidth;
+	onPointerMove = (event) => {
+		this.resize(event.clientX - this.resizerEl.getBoundingClientRect().left);
+	};
 
-		if (draggedWidth > maxWidth) {
+	resize = (newWidth) => {
+		const maxWidth = parseFloat(this.resizerEl.style.maxWidth);
+
+		if (newWidth > maxWidth) {
 			newWidth = maxWidth;
 		}
 
-		if (draggedWidth < 16) {
+		if (newWidth < 16) {
 			// 16 to make sure the lilac handle stays visible.
 			newWidth = 16;
 		}
 
-		resizerEl.style.width = `${newWidth}px`;
+		console.log(newWidth);
+		this.resizerEl.style.width = `${newWidth}px`;
 		// The dragger is 24px wide, deducting 12 ensures that it's centered on the visible line.
-		draggerEl.style.left = `${newWidth - 12}px`;
+		this.draggerEl.style.left = `${newWidth - 12}px`;
+	};
+
+	onDraggerKeyDown = (event) => {
+		let resizerWidth = this.resizerEl.getBoundingClientRect().width;
+
+		switch (event.key) {
+			case "ArrowLeft":
+				this.resize(resizerWidth - 16);
+				break;
+			case "ArrowRight":
+				this.resize(resizerWidth + 16);
+				break;
+		}
 	};
 }
 
